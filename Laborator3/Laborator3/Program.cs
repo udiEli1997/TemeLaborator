428c9ba4838c399b8820baf7440a9a34c7f72e14
udiEli1997 <udisteanu.iulian@outlook.com>
2020-03-14 13:48:42 +0200

    Laborator 3.1

--- Laborator3/Laborator3/Program.cs
+++ Laborator3/Laborator3/Program.cs
@@ -10,7 +10,31 @@ namespace Laborator3
     {
         static void Main(string[] args)
         {
-
+            string[][] tablou = new string[26][];
+            int[] dimensiuni = new int[26];
+            for(int i = 0; i < 26; i++)
+            {
+                tablou[i] = new string[10];
+                
+            }
+            foreach(var cuvant in args)
+            {
+                int index = (int)cuvant.ToLower()[0] - 97;
+                tablou[index][dimensiuni[index]] = cuvant;
+                dimensiuni[index]++;
+            }
+            for(int i = 0; i < 26; i++)
+            {
+                for(int j = 0; j < dimensiuni[i]; j++)
+                {
+                    Console.Write("{0} ", tablou[i][j]);
+                }
+                if (dimensiuni[i] != 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
